d654f0419e18c39756d599d0995df6d2bb2e5492
noman-swe <noman.swe130@gmail.com>
2022-12-21 00:41:09 +0600

    condition updated but icon left

--- Extra-class-37_5/temp-hot-solution/js/app.js
+++ Extra-class-37_5/temp-hot-solution/js/app.js
@@ -1,15 +1,28 @@
 const apiKey = `def41e2a02f862100531f8648775e26f`;
 
+document.getElementById('city-name').addEventListener('keypress', function (e) {
+    if (e.key == 'Enter') {
+        searchTemperature();
+    }
+});
+
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayTemperature(data));
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
 };
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
 
 const displayTemperature = temperature => {
     console.log(temperature);
     console.log(temperature.name);
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
 }
