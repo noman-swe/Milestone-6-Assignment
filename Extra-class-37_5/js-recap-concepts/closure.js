ed4201cb64e07b20f516cbf8f38dd385b35043dc
noman-swe <noman.swe130@gmail.com>
2022-12-21 23:32:51 +0600

    closure done

--- Extra-class-37_5/js-recap-concepts/closure.js
+++ Extra-class-37_5/js-recap-concepts/closure.js
@@ -1,15 +1,36 @@
-const bank = owner => {
+/* const bank = owner => {
     balance = 0;
     return amount => {
         // balance = balance + amount;
         balance += amount;
         return balance;
     }
+} */
+
+//-> kono ekta function theke jodi tmi onno kono ekta  function k OR function er vitor theke jodi arekta function k return koro tahole function er erternal kisu peramiter or property k access and modify kora possible but baire theke tar kisui possible na.
+
+const bank = owner => {
+    balance = 0;
+    return {
+        deposit:amount => {
+            // balance = balance + amount;
+            balance += amount;
+            return balance;
+        },
+        withdraw:amount => {
+            // balance = balance + amount;
+            balance -= amount;
+            return balance;
+        }
+    }
 }
 
 const mofizBank = bank('Mofiz');
-console.log(mofizBank(100));
-console.log(mofizBank(300));
-console.log(mofizBank(50));
-console.log(mofizBank.balance);
-console.log(mofizBank(200));
+console.log(mofizBank.deposit(100));
+console.log(mofizBank.deposit(300));
+console.log(mofizBank.deposit(50));
+// console.log(mofizBank.deposit.balance);
+console.log(mofizBank.deposit(200));
+
+console.log(mofizBank.withdraw(100));
+console.log(mofizBank.withdraw(200));
